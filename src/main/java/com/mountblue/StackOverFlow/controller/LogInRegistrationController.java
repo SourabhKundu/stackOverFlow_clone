da5de879c05bfd82f32f76622d6dfbde73de7c5d
Vinayak kamati <vinayakkamati123@gmail.com>
2021-10-17 23:29:07 +0530

    created the methods for registration in controller

--- src/main/java/com/mountblue/StackOverFlow/controller/LogInRegistrationController.java
+++ src/main/java/com/mountblue/StackOverFlow/controller/LogInRegistrationController.java
@@ -1,17 +1,43 @@
 package com.mountblue.StackOverFlow.controller;
 
+import com.mountblue.StackOverFlow.service.UserService;
+import com.mountblue.StackOverFlow.userDto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LogInRegistrationController {
 
-    @GetMapping("/registration")
-    public String createNewUser() {
-        return "registration";
-    }
-    @GetMapping("/login2")
+    private final UserService userService;
+
+
+    @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @ModelAttribute("user")
+    public UserDto userDto() {
+        return new UserDto();
+    }
+
+    public LogInRegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public String createNewUser(Model model) {
+        model.addAttribute("user", new UserDto());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("user") UserDto userDto) {
+        userService.saveUserDetails(userDto);
+        return "registration";
     }
 }
